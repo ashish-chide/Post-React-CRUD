ddfc8f8ed983ac91f81a256816ef5236d4430cea
ashish.chide <ashish.chide@wgblindia.com>
2019-04-22 18:27:48 +0530

    completed!

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,9 +13,7 @@ class FullPost extends Component {
         !this.state.loadedPost ||
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
       ) {
-        Axios.get(
-          "https://jsonplaceholder.typicode.com/posts/" + this.props.id
-        ).then(response => {
+        Axios.get("/posts/" + this.props.id).then(response => {
           this.setState({ loadedPost: response.data });
         });
       }
@@ -23,9 +21,7 @@ class FullPost extends Component {
   }
 
   postDeleteHandler = () => {
-    Axios.delete(
-      "https://jsonplaceholder.typicode.com/posts/" + this.props.id
-    ).then(response => {
+    Axios.delete("/posts/" + this.props.id).then(response => {
       console.log(response);
     });
   };
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -15,11 +15,9 @@ class NewPost extends Component {
       body: this.state.content,
       author: this.state.author
     };
-    Axios.post("https://jsonplaceholder.typicode.com/posts", data).then(
-      response => {
-        console.log(response);
-      }
-    );
+    Axios.post("/posts", data).then(response => {
+      console.log(response);
+    });
   };
 
   render() {
